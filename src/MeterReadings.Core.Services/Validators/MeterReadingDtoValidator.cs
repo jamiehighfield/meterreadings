86d32fb9d901b3f83736cd826153f8d8effdb03a
Jamie Highfield <jamie@jamiehighfield.co.uk>
2021-12-12 19:55:44 +0000

    Moved meter reading validator to its own file.

---
+++ src/MeterReadings.Core.Services/Validators/MeterReadingDtoValidator.cs
@@ -0,0 +1,23 @@
+﻿using FluentValidation;
+
+namespace MeterReadings.Core.Services
+{
+    /// <summary>
+    /// Validator for <see cref="MeterReadingDto"/>.
+    /// </summary>
+    public class MeterReadingDtoValidator : AbstractValidator<MeterReadingDto>
+    {
+        /// <summary>
+        /// Initialise a new instance of <see cref="MeterReadingDtoValidator"/>.
+        /// </summary>
+        public MeterReadingDtoValidator()
+        {
+            RuleFor((dto) => dto.Value)
+                .Must((value) => value.Length == 5)
+                .Must((value) =>
+                {
+                    return int.TryParse(value, out int number) && number > 0 && number < 99999;
+                });
+        }
+    }
+}
