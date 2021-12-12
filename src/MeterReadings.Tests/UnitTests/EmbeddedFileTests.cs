c44aa64670df55a905a011164e30a3ffbcba43f6
Jamie Highfield <jamie@jamiehighfield.co.uk>
2021-12-12 19:38:34 +0000

    Added the InternalsVisibleTo attribute in the data access project so that it could be properly tested.

---
+++ src/MeterReadings.Tests/UnitTests/EmbeddedFileTests.cs
@@ -0,0 +1,20 @@
+﻿using MeterReadings.DataAccess;
+using Xunit;
+
+namespace MeterReadings.Tests.UnitTests
+{
+    public class EmbeddedFileTests
+    {
+        /// <summary>
+        /// This test determines whether the embedded file reader in the data access layer works as intended.
+        /// </summary>
+        [Fact]
+        public void CanReadEmbeddedFile()
+        {
+            string fileContents = EmbeddedFile.Read("Seeds.developmentseed.sql");
+
+            Assert.NotNull(fileContents);
+            Assert.NotEqual("", fileContents);
+        }
+    }
+}
