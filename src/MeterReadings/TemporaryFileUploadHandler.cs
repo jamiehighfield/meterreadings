b9df722b89ca1d417f19fbe2e7ea0086a2b0b865
Jamie Highfield <jamie@jamiehighfield.co.uk>
2021-12-12 14:57:07 +0000

    Helper class to facilitate uploading files from an HTTP client without needing to store them in memory for the duration of the request.

---
+++ src/MeterReadings/TemporaryFileUploadHandler.cs
@@ -0,0 +1,75 @@
+﻿namespace MeterReadings
+{
+    /// <summary>
+    /// Used to store files uploaded and stored in memory into temporary file storage.
+    /// </summary>
+    public class TemporaryFileUploadHandler
+    {
+        /// <summary>
+        /// Handles an uploaded file from an instance of <see cref="IFormFile"/> and stores the uploaded file in temporary file storage, returning an instance of <see cref="UploadedFile"/> that can be used to interface with the file.
+        /// </summary>
+        /// <param name="file">The file that has been uploaded.</param>
+        /// <returns>An instance of <see cref="UploadedFile"/>.</returns>
+        public async Task<UploadedFile> HandleUploadAsync(IFormFile file)
+        {
+            if (file is null)
+            {
+                throw new ArgumentNullException(nameof(file));
+            }
+
+            var temporaryFile = Path.GetTempFileName();
+
+            using (Stream fileStream = File.Create(temporaryFile))
+            {
+                await file.CopyToAsync(fileStream);
+            }
+
+            return new UploadedFile(temporaryFile);
+        }
+    }
+
+    /// <summary>
+    /// Represents a file uploaded and stored in temporary file storage.
+    /// </summary>
+    public class UploadedFile : IDisposable
+    {
+        /// <summary>
+        /// Initialise a new instance of <see cref="UploadedFile"/>.
+        /// </summary>
+        /// <param name="filePath">The temporary path of the file.</param>
+        public UploadedFile(string filePath)
+        {
+            if (string.IsNullOrEmpty(filePath))
+            {
+                throw new ArgumentException($"'{nameof(filePath)}' cannot be null or empty.", nameof(filePath));
+            }
+
+            _filePath = filePath;
+        }
+
+        private string _filePath;
+        private Stream _stream;
+
+        /// <summary>
+        /// Opens a pointer to the file as a stream.
+        /// </summary>
+        /// <returns>An instance of <see cref="Stream"/> pointing to the start of the file.</returns>
+        public Stream Open()
+        {
+            _stream = File.Open(_filePath, FileMode.Open);
+
+            return _stream;
+        }
+
+        /// <summary>
+        /// <inheritdoc/>
+        /// </summary>
+        /// <exception cref="NotImplementedException"></exception>
+        public void Dispose()
+        {
+            if (_stream != null) _stream.Dispose();
+
+            if (File.Exists(_filePath)) File.Delete(_filePath);
+        }
+    }
+}
