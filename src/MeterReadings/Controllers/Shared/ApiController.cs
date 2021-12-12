221b8ad123cdf1228beed1eb76acb1d6eaba9362
Jamie Highfield <jamie@jamiehighfield.co.uk>
2021-12-12 14:53:02 +0000

    Base class for an API controller. This provides some mechanisms that are more suited for JSON-based APIs. For example, the base controller applies the API exception filter also included in this commit.

---
+++ src/MeterReadings/Controllers/Shared/ApiController.cs
@@ -0,0 +1,125 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Runtime.Serialization;
+using System.Text.Json.Serialization;
+
+namespace MeterReadings.Controllers
+{
+    /// <summary>
+    /// Extend this class to indicate that the extending class is an API controller.
+    /// </summary>
+    [ApiExceptionFilter]
+    public abstract class ApiController : Controller
+    {
+        /// <summary>
+        /// Returns a success response with the specified data.
+        /// </summary>
+        /// <typeparam name="TPayloadType"></typeparam>
+        /// <param name="payload"></param>
+        /// <returns></returns>
+        public ActionResult<ApiResponse> Success<TPayloadType>(TPayloadType payload)
+            where TPayloadType : class => Json(new SuccessfulApiResponse<TPayloadType>(payload));
+    }
+
+    /// <summary>
+    /// A standardised schema for responding to API requests.
+    /// </summary>
+    public abstract class ApiResponse
+    {
+        /// <summary>
+        /// Initialise a new instance of <see cref="ApiResponse"/>.
+        /// </summary>
+        /// <param name="success">A <see cref="bool"/> value indicating whether or not the request was successful.</param>
+        protected ApiResponse(bool success)
+        {
+            Success = success;
+        }
+
+        /// <summary>
+        /// Gets or sets a <see cref="bool"/> value indicating whether or not the request was successful.
+        /// </summary>
+        [JsonPropertyName("success")]
+        public bool Success { get; }
+    }
+
+    /// <summary>
+    /// A standardised schema for responding to successful API requests.
+    /// </summary>
+    /// <param name="success">A <see cref="bool"/> value indicating whether or not the request was successful.</param>
+    public class SuccessfulApiResponse : ApiResponse
+    {
+        /// <summary>
+        /// Initialise a new instance of <see cref="SuccessfulApiResponse"/>.
+        /// </summary>
+        public SuccessfulApiResponse(bool success)
+            : base(true) { }
+    }
+
+    /// <summary>
+    /// A standardised schema for responding to successful API requests.
+    /// </summary>
+    /// <typeparam name="TPayloadType">The type of payload.</typeparam>
+    public class SuccessfulApiResponse<TPayloadType> : SuccessfulApiResponse
+        where TPayloadType : class
+    {
+        /// <summary>
+        /// Initialise a new instance of <see cref="SuccessfulApiResponse{TPayloadType}"/>.
+        /// </summary>
+        /// <param name="payload">The response payload data as an instance of <typeparamref name="TPayloadType"/>.</param>
+        public SuccessfulApiResponse(TPayloadType payload)
+            : base(true)
+        {
+            Payload = payload ?? throw new ArgumentNullException(nameof(payload));
+        }
+
+        /// <summary>
+        /// Gets the response payload data as an instance of <typeparamref name="TPayloadType"/>.
+        /// </summary>
+        [JsonPropertyName("payload")]
+        public TPayloadType Payload { get; }
+    }
+
+    /// <summary>
+    /// A standardised schema for responding to unsuccessful API requests.
+    /// </summary>
+    public class UnsuccessfulApiResponse : ApiResponse
+    {
+        /// <summary>
+        /// Initialise a new instance of <see cref="UnsuccessfulApiResponse"/>.
+        /// </summary>
+        public UnsuccessfulApiResponse()
+            : base(false)
+        {
+            ErrorMessage = GetErrorResponse();
+        }
+
+        /// <summary>
+        /// Initialise a new instance of <see cref="UnsuccessfulApiResponse"/>.
+        /// </summary>
+        /// <param name="exception">The exception that was thrown.</param>
+        public UnsuccessfulApiResponse(Exception exception)
+            : base(false)
+        {
+            Exception = exception;
+
+            ErrorMessage = GetErrorResponse();
+        }
+
+        /// <summary>
+        /// Gets the exception that was thrown.
+        /// </summary>
+        [IgnoreDataMember]
+        protected Exception Exception { get; }
+
+        /// <summary>
+        /// Gets the error message.
+        /// </summary>
+        [JsonPropertyName("error")]
+        public string ErrorMessage { get; }
+
+        /// <summary>
+        /// Gets the error response to be included in the API response.
+        /// </summary>
+        /// <returns>The error response to be included in the API response.</returns>
+        protected virtual string GetErrorResponse() => "Unknown error";
+    }
+}
---
+++ src/MeterReadings/Filters/ApiExceptionFilterAttribute.cs
@@ -0,0 +1,28 @@
+﻿using MeterReadings.Controllers;
+using MeterReadings.Shared.Exceptions;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Filters;
+
+namespace MeterReadings
+{
+    /// <summary>
+    /// Apply this attribute to catch standard exceptions and alter the HTTP response as appropriate.
+    /// </summary>
+    public class ApiExceptionFilterAttribute : ExceptionFilterAttribute
+    {
+        public ApiExceptionFilterAttribute() { }
+
+        public override void OnException(ExceptionContext context)
+        {
+            switch (context.Exception)
+            {
+                case EntityNotFoundException:
+                    context.Result = new NotFoundResult();
+                    break;
+                case Exception:
+                    context.Result = new JsonResult(new UnsuccessfulApiResponse(context.Exception));
+                    break;
+            }
+        }
+    }
+}
