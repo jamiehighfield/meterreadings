665c0a25c9013c0b272697d6a2b070f7bcc56f5e
Jamie Highfield <jamie@jamiehighfield.co.uk>
2021-12-12 14:51:52 +0000

    Migrations and seed files based on the data access structures previous completed. This includes: - initial migration - clear database script (for resetting on each dev run) - development seed - testing seed

---
+++ src/MeterReadings.DataAccess/Migrations/11.12.2021.1.InitialMigration.cs
@@ -0,0 +1,36 @@
+﻿using SimpleMigrations;
+
+namespace MeterReadings.DataAccess.Migrations
+{
+    [Migration(1, "Initial migration")]
+    public class InitialMigration : Migration
+    {
+        protected override void Up()
+        {
+            Execute(@"
+                CREATE TABLE accounts
+                (
+                    id         BIGSERIAL CONSTRAINT accounts_pk PRIMARY KEY,
+                    account_id BIGINT NOT NULL,
+                    first_name VARCHAR(128),
+                    last_name  VARCHAR(128) NOT NULL
+                );
+
+                CREATE UNIQUE INDEX accounts_id_uindex
+                    ON accounts (id);
+
+                CREATE TABLE meter_readings
+                (
+                    id          BIGSERIAL CONSTRAINT meter_readings_pk PRIMARY KEY,
+                    account_id   BIGINT NOT NULL,
+                    submitted_at TIMESTAMP NOT NULL,
+                    value       VARCHAR(5) NOT NULL
+                );
+
+                CREATE UNIQUE INDEX meter_readings_id_uindex
+                    ON meter_readings (id);");
+        }
+
+        protected override void Down() { }
+    }
+}
---
+++ src/MeterReadings.DataAccess/Sql/cleardatabase.sql
@@ -0,0 +1,15 @@
+﻿DO
+$func$
+BEGIN
+	IF NOT EXISTS (select * from pg_tables where schemaname = 'public') THEN
+		RETURN;
+	END IF;
+
+	EXECUTE (SELECT 'DROP TABLE IF EXISTS '
+		|| string_agg(quote_ident(schemaname) || '.' || quote_ident(tablename), ', ')
+		|| ' CASCADE'
+	FROM   pg_tables
+	WHERE  (schemaname = 'public')
+	);
+END
+$func$;
